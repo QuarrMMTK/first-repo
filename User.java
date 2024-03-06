8436e77f55f29ff5648ab3f2996674d7a5ca6fa0
QuarrMMTK <134800254+QuarrMMTK@users.noreply.github.com>
2024-03-06 13:59:04 +0630

    Add files via upload

---
+++ User.java
@@ -0,0 +1,58 @@
+package com.java.bean;
+
+import java.util.Date;
+
+public class User {
+	private int id;
+	private String username;
+	private String email;
+	private String password;
+	private int role;
+	private Date date;
+	private String roleName;
+	
+	
+	
+	public String getRoleName() {
+		return roleName;
+	}
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public int getRole() {
+		return role;
+	}
+	public void setRole(int role) {
+		this.role = role;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
